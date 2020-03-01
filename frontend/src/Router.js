fc98deb580445bffe341d2b669816f222323fd55
EChColorado <edxz7c@gmail.com>
2020-02-29 19:02:12 -0600

    concierge first stage done

--- frontend/src/Router.js
+++ frontend/src/Router.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import Home from './components/home/Home';
-import NotFound from './components/404/NotFound.js';
+import Home from './pages/home/Home';
+import Concierge from './pages/concierge/Concierge';
+// import NotFound from './components/404/NotFound.js';
 
 const Router = () => (
   <BrowserRouter>
     <Switch>
       <Route exact path="/" component={Home} />
-      <Route component={NotFound} />
+      <Route exact path='/con' component={Concierge} />
+      {/* <Route component={NotFound} /> */}
     </Switch>
   </BrowserRouter>
 );
